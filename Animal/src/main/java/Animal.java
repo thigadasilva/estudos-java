6efee06e48b78ffa1f500fd635d2835215c91d6c
Thiago da Silva <thiggasto@gmail.com>
2025-03-07 10:13:11 -0300

    Primeiro código de herança

---
+++ Animal/src/main/java/Animal.java
@@ -0,0 +1,34 @@
+public class Animal {
+    private String nome;
+    private String raca;
+
+    public Animal(String nome, String raca){
+        this.nome = nome;
+        this.raca = raca;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getRaca(){
+        return raca;
+    }
+    public void setRaca(String raca){
+        this.raca = raca;
+    }
+
+    public void imprimirAnimal(){
+        System.out.println("Nome: " + getNome());
+        System.out.println("Raça: " + getRaca());
+    }
+
+    public void caminhar(){
+        System.out.println("Seu animal caminhou por 5 metros!");
+    }
+
+
+
+}
