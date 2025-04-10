a641fb4bb1cc76e0ab3b857fd5bacfa7b9d2bf56
Thiago da Silva <thiggasto@gmail.com>
2025-04-10 09:33:28 -0300

    Correção do código

--- Agregação e Composição/atividade4_PC/src/main/java/Main.java
+++ Agregação e Composição/atividade4_PC/src/main/java/Main.java
@@ -1,16 +1,16 @@
-public class Main {
+public class main {
 
     public static void main(String[] args){
 
-        DiscoRigido tres = new DiscoRigido("HD", 1000);
-        Memoria dois = new Memoria(16);
-        Processador um = new Processador("Ryzen", 10000);
+                DiscoRigido tres = new DiscoRigido("HD", 1000);
+                Memoria dois = new Memoria(16);
+                Processador um = new Processador("Ryzen", 10000);
 
-        Computador pichau = new Computador(um, dois, tres);
+                Computador pichau = new Computador(um, dois, tres);
 
-        System.out.println(um.getNome());
-        System.out.println(dois.getCapacidade());
-        System.out.println(tres.getTipo() + tres.getCapacidade());
+                System.out.println(pichau.getProcessador().getNome());
+                System.out.println(pichau.getDiscoRigido().getCapacidade() + pichau.getDiscoRigido().getTipo());
+                System.out.println(pichau.getMemoria().getCapacidade());
 
     }
 }
